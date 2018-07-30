be94e225e311f4f1fb27801b6c31ec92886a3bc3
Kevin Kabore <kevinkabore@Kevins-iMac.local>
2018-07-30 15:17:31 -0700

    adds some tweaks suggested by tlighthouse

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,6 +5,7 @@ const SearchBox = ({ searchChange, searchfield }) => {
   return (
     <div className="pa2">
       <input
+        aria-label='Search Robots'
         className="pa3 ba b--green bg lightest-blue"
         type="search"
         placeholder="search robot homies"
